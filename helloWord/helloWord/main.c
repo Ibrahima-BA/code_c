80f39c8bc4581d6cb04f74c5c709fe5a1efdceb4
Ibrahima BA <iba99@icloud.com>
2024-04-18 15:06:37 +0200

    First comit

---
+++ helloWord/helloWord/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  helloWord
+//
+//  Created by Ibrahima BA on 18/04/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
