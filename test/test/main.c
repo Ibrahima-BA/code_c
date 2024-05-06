b77fc1dfae602ac42696df06cb8071d07ee0f8ab
Ibrahima BA <iba99@icloud.com>
2024-05-06 11:42:04 +0200

    MISE A JOUR

---
+++ test/test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test
+//
+//  Created by Ibrahima BA on 22/04/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
